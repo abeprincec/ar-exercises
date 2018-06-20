ca620c87ced5480b80d55c38962dbddc682dee5d
abeprincec <abeprincec@gmail.com>
2018-06-20 16:12:56 +0000

    finish exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,15 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: "T", womens_apparel: "T")
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: "F", womens_apparel: "T")
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: "T", womens_apparel: "F")
+
+
 # Your code goes below here ...
+
+puts Store.count
--- setup.rb
+++ setup.rb
@@ -8,6 +8,7 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
+  
   adapter: 'postgresql',
   database: 'ar_exercises',
   username: 'development',
