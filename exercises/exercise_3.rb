6d7dc10c266feea8ab39da2b0db1d496b023bef4
abeprincec <abeprincec@gmail.com>
2018-06-20 16:50:06 +0000

    finish exercise 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,4 +8,5 @@ puts "----------"
 @store3 = Store.find_by(id: 3)
 
 @store3.destroy()
-# Your code goes here ...
+
+puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+burnaby = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: "T", womens_apparel: "F")
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: "T", womens_apparel: "F")
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: "T", womens_apparel: "T")
+
+@mens_stores= Store.where(mens_apparel: "T")
+
+#print @mens_stores.pluck(:name, :annual_revenue)
+
+@women_stores_1m = Store.where(womens_apparel: "T").where("annual_revenue < ?", 1000000)
+
+puts @women_stores_1m
